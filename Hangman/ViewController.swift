787721171b6ece45a50dd8e0f23069427c5c96a0
Noah Braunfeld <noahbraunfeld@gmail.com>
2020-01-16 10:10:08 -0500

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Noah Braunfeld (student HH) on 1/16/20.
+//  Copyright © 2020 Noah Braunfeld (student HH). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
